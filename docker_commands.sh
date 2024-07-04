f4af12844b30dbce94d932d11180eb03d2f690b0
Ktulchu <roma442006@yandex.ru>
2024-07-04 16:29:02 +0300

    posgras settings

--- docker_commands.sh
+++ docker_commands.sh
@@ -5,22 +5,16 @@ docker compose run --rm php composer cr
 
 # Проверка успешного завершения установки Yii2
 if [ $? -eq 0 ]; then
-  # Если успешно, поднимаем все остальные контейнеры
-  docker compose up -d
+    # Если успешно, поднимаем все остальные контейнеры
+    docker compose up -d
 
-  # проверяем запуск контейнеров
-  if [ $? -eq 0 ]; then
-    # добавляем библиотеку
-    docker composer require vlucas/phpdotenv
+    # проверяем запуск контейнеров
     if [ $? -eq 0 ]; then
-      # По завершению запускаем миграции
-      docker compose run --rm php php yii migrate
+        # По завершению запускаем миграции
+        docker compose run --rm php php yii migrate
     else
-      echo "Ошибка запусков контрейнеров"
+        echo "Ошибка запусков контрейнеров"
     fi
-  else
-    echo "Ошибка Yii2 framework"
-  fi
 else
-  echo "Ошибка установки Yii2 framework"
+    echo "Ошибка установки Yii2 framework"
 fi
