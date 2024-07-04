aa0d4a479ffdc336d7e75c9213f4184bd9d6939a
Ktulchu <roma442006@yandex.ru>
2024-07-04 15:51:48 +0300

    bash update

--- docker_commands.sh
+++ docker_commands.sh
@@ -5,18 +5,22 @@ docker compose run --rm php composer cr
 
 # Проверка успешного завершения установки Yii2
 if [ $? -eq 0 ]; then
-    # Если успешно, поднимаем все остальные контейнеры
-    docker compose up -d
+  # Если успешно, поднимаем все остальные контейнеры
+  docker compose up -d
 
-    # проверяем запуск контейнеров
+  # проверяем запуск контейнеров
+  if [ $? -eq 0 ]; then
+    # добавляем библиотеку
+    docker composer require vlucas/phpdotenv
     if [ $? -eq 0 ]; then
-        # добавляем библиотеку
-        docker composer require vlucas/phpdotenv
-        # По завершению запускаем миграции
-        docker compose run --rm php php yii migrate
+      # По завершению запускаем миграции
+      docker compose run --rm php php yii migrate
     else
-        echo "Ошибка запусков контрейнеров"
+      echo "Ошибка запусков контрейнеров"
     fi
+  else
+    echo "Ошибка Yii2 framework"
+  fi
 else
-    echo "Ошибка установки Yii2 framework"
+  echo "Ошибка установки Yii2 framework"
 fi
